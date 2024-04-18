e13e263583d65221cdb2eca504c2518cdcf5b05d
VITALY-VORON <as7950203@gmail.com>
2024-04-18 13:45:08 +0300

    .

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,7 +1,6 @@
 import {
   Controller,
   Post,
-  Body,
   UseInterceptors,
   UploadedFile,
   ParseFilePipe,
@@ -10,9 +9,10 @@ import {
   UseGuards,
   Query,
   Delete,
+  Param,
+  Res,
 } from '@nestjs/common';
 import { FilesService } from './files.service';
-import { CreateFileDto } from './dto/create-file.dto';
 import { ApiBearerAuth, ApiBody, ApiConsumes, ApiTags } from '@nestjs/swagger';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { fileStorage } from './storage';
@@ -20,12 +20,16 @@ import { JwtAuthGuard } from '../auth/guards/jwt.guard';
 import { UserId } from '../decorators/user-id.decorator';
 import { FileType } from './entities/file.entity';
 
+import * as fs from 'fs';
+import { Response } from 'express';
+import * as path from 'path';
+
 @Controller('files')
 @ApiTags('files')
 @UseGuards(JwtAuthGuard)
 @ApiBearerAuth()
 export class FilesController {
-  constructor(private readonly filesService: FilesService) {}
+  constructor(private readonly filesService: FilesService) { }
 
   @Get()
   findAll(@UserId() userId: number, @Query('type') fileType: FileType) {
@@ -64,7 +68,34 @@ export class FilesController {
 
   @Delete()
   remove(@UserId() userId: number, @Query('ids') ids: string) {
-    // files?ids=1,2,7,8
     return this.filesService.remove(userId, ids);
+  }
+
+  @Get('/one/:fileName')
+  parseJsonData(@Res() res: Response, @Param('fileName') fileName: string) {
+    const filePath = 'uploads'
+    res.set({
+      'Content-Disposition': `attachment; filename=${fileName}`,
+      'Content-Type': 'application/json; charset=utf-8',
+    })
+    return fs.createReadStream(filePath).pipe(res)
+  }
+
+  @Get('/all')
+  async downloadAllFiles(@Res() res: Response) {
+    const directoryPath = 'uploads';
+    const files = await fs.promises.readdir(directoryPath);
+
+    res.set({
+      'Content-Type': 'application/octet-stream',
+    });
+
+    for (const file of files) {
+      const filePath = path.join(directoryPath, file);
+      if ((await fs.promises.stat(filePath)).isFile()) {
+        res.attachment(file);
+        fs.createReadStream(filePath).pipe(res);
+      }
+    }
   }
 }
